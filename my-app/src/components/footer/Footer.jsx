da708a529e55ced3a3f8ea9871132b1df4ab0fe0
Aaron Portuphy <aaronportuphy@gmail.com>
2022-08-13 16:58:29 +0000

    Initial Commit

---
+++ my-app/src/components/footer/Footer.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import "./footer.css"
+import { blog } from '../../dummydata'
+
+const Footer = () => {
+  return (
+    <>
+        <section className='newletter'>
+            <div className="container flexSB">
+                <div className="left row">
+                    <h1>Newsletter - Stay tune ang get the latest </h1>
+                    <span>far far away, behind the word mountain</span>
+                </div>
+                <div className="right row">
+                    <input type='text' placeholder='Enter email address' />
+                    <i className='fa fa-paper-plane'></i>
+                </div>
+            </div>
+        </section>
+
+        <footer>
+            <div className="container padding">
+                <div className="box logo">
+                    <h1>JOBSPOT</h1>
+                    <span>online eucation & learning</span>
+                    <p> Voluptas a voluptatem eaos necessitatibus fugit commodi tenetur odio expedita amet debitis ducimus laudantium ullam ut corporis saepe inventore assumenda accusamus error.</p>
+                    <i className='fab fa-facebook-f icon'></i>
+                    <i className='fab fa-instagram icon'></i>
+                    <i className='fab fa-twitter icon'></i>
+                    <i className='fab fa-youtube icon'></i>
+                </div>
+                <div className="box link">
+                    <h3>Explore</h3>
+                    <ul>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+                <div className="box link">
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+                <div className="bxo">
+                    <h3>Recent Post</h3>
+                    {blog.slice(0,3).map((val) =>{
+                        return(
+                            <div className="items flexSB">
+                                <div className="img">
+                                    <img src={val.cover} alt='' />
+                                </div>
+                                <div className="text">
+                                <span>
+                                   <i className='fa fa-user'></i>
+                                  <label htmlFor="">{val.type}</label>
+                                 </span>
+                                 <span>
+                                     <i className='fa fa-calendar-alt'></i>
+                                    <label htmlFor="">{val.date}</label>
+                                 </span>
+                                 <h4>{val.title}</h4>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="box last">
+                    <h3>Have a Question?</h3>
+                    <ul>
+                        <li>
+                            <i className='fa fa-map'></i>
+                            203 Fake ST. Mount View, san francisco california, USA
+                        </li>
+                        <li>
+                            <i className='fa fa-phone-alt'></i>
+                            +123 456 7897
+                        </li>
+                        <li>
+                            <i className='fa fa-paper-plane'></i>
+                            example@gmail.com
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+        <div className="legal">
+            <p>
+                Copyright @2022 All rights reserved | This template is made with <i className='fa fa-heart'></i> by Aaron
+            </p>
+        </div>
+    </>
+  )
+}
+
+export default Footer
